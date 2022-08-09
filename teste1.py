315c6e0a28894fd65f68682b803b3136ea01f12d
Lara Toledo <110473452+laratoledom@users.noreply.github.com>
2022-08-09 19:50:06 -0300

    Update teste1.py

--- teste1.py
+++ teste1.py
@@ -1,1 +1,14 @@
 # SOMA
+#Trazendo o código para a Linguagem Python temos:
+
+indice = 13
+soma = 0
+K = 0
+
+while K < indice:
+    K = K + 1
+    soma = soma + K
+
+print(soma)
+
+# Nesse caso o resultado da soma = 91
